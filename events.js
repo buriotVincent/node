9c54fd83b7deff257e6aa060816c770abfaf8b97
buriotVincent <buriot.vincent@gmail.com>
2019-06-03 15:45:34 +0200

    test events 2

--- events.js
+++ events.js
@@ -9,6 +9,16 @@ server.on('close', function() {
     console.log('Bye bye !');
 });
 
+var EventEmitter = require('events').EventEmitter;
+
+var jeu = new EventEmitter();
+
+jeu.on('gameover', function(message, age){
+    console.log(message, age);
+});
+
+jeu.emit('gameover', 'Vous avez perdu', '\nVous avez ' + 32 + ' ans');
+
 server.listen(3389);
 
 server.close();
