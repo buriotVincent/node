f9cdf0e4c6ef9127509e1ce806ebff9d9b57e2a6
buriotVincent <buriot.vincent@gmail.com>
2019-06-03 16:31:02 +0200

    test events 4

--- app.js
+++ app.js
@@ -2,3 +2,7 @@ var monmodule = require('./monmodule');
 
 monmodule.direBonjour();
 monmodule.direAurevoir();
+
+var markdown = require('markdown').markdown;
+
+console.log(markdown.toHTML('Un paragraphe en **markdown** !'));
