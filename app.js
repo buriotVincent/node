fdeee416d9c656724da862e011fa2f886f233af8
buriotVincent <buriot.vincent@gmail.com>
2019-06-03 13:45:48 +0200

    test

--- app.js
+++ app.js
@@ -1,2 +1,5 @@
-console.log('coucou2');
 
+request('http://www.site.com/fichier.zip', function (error, response, body) {
+    console.log("Fichier téléchargé !");
+});
+console.log("Je fais d'autres choses en attendant...");
